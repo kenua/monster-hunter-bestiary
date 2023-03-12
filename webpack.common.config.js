677899a8393c0c598edf72425bfe22ff6e4ddc42
Kenua <jgvezon@gmail.com>
2023-03-12 10:59:30 -0500

    Add configuration file for production

---
+++ webpack.common.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+   entry: {
+      index: path.resolve(__dirname, 'src/index.js'),
+      admin: path.resolve(__dirname, 'src/admin.js'),
+   },
+   output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: true,
+   },
+   devServer: {
+      static: './dist',
+   },
+   module: {
+      rules: [
+         {
+            test: /\.s[ac]ss$/i,
+            use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+            ],
+         },
+         {
+            test: /\.html$/i,
+            loader: "html-loader",
+         },
+      ],
+   },
+   plugins: [
+      new HtmlWebpackPlugin({
+         chunks: ['index'],
+         filename: 'index.html',
+         template: path.resolve(__dirname, 'src/pages/index.html'),
+      }),
+      new HtmlWebpackPlugin({
+         chunks: ['admin'],
+         filename: 'admin.html',
+         template: path.resolve(__dirname, 'src/pages/admin.html'),
+      }),
+   ],
+};
